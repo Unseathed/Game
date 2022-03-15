4134c14ffa7ac5cb3f6294a5812c3d85e724d66c
Unseathed <99050807+Unseathed@users.noreply.github.com>
2022-03-15 05:33:30 -0400

    Function

---
+++ My project (11)/Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private const double V = .1;
+    public float speed = (float)V;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * speed;
+    }
+}
---
+++ My project (11)/Assets/Scripts/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject WinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy(gameObject);
+
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Destroy(gameObject);
+
+           // rb.AddForce(Vector3.up * 500);
+        }
+
+        //rb.velocity = Vector3.forward * 20f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level2");
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag =="Enemy")
+        {
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+            WinText.SetActive(true);
+        }
+        
+    }
+
+
+
+
+
+
+
+
+}
